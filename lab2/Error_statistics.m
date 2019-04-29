7a929afc2fc7febfa9baba49f5189ee2e1631ee4
mlx <32833871+lingxiaomeng@users.noreply.github.com>
2019-04-29 09:46:44 +0800

    Create Error_statistics.m

---
+++ lab2/Error_statistics.m
@@ -0,0 +1,18 @@
+clc
+clear
+err=zeros(1,20);
+for n=1:1:100  %Calculate the error of different segements
+    V2=Integral();
+    V1=Infinitesimal(n);
+    error=abs(V2-V1);
+    err(n)=sum(sum(error));
+end
+err=err/100/100;
+
+n=1:1:100;
+figure(1)
+plot(n,err)
+title({'The relationship between error and number of segments','-integration','11711913 孟令啸'},'fontsize',20) ;%title the figure
+ylabel('Average Error','fontsize',20);  %label the y axis
+
+cftool(n,err) %curve fitting
